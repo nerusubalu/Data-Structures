c260d8e9e8021bdeb053a28bd0ea29ca81f27bbe
nerusubalu <nerusubalu@gmail.com>
2021-06-13 19:45:08 +0530

    Added Linear Search

---
+++ Searching Algorithms/1 Linear Search/Linear Search.cpp
@@ -0,0 +1,30 @@
+// C++ implementation of Linear Search
+#include <bits/stdc++.h>
+using namespace std;
+
+// Linearly search x in arr[].
+// If x is present then return the index,
+// otherwise return -1
+int search(int arr[], int n, int x)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = {1, 10, 30, 15};
+    int x = 30;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << x << " is present at index "
+         << search(arr, n, x);
+
+    getchar();
+    return 0;
+}
