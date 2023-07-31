57dcaf6a7104a11e8dfb4493a09e804e420188fd
nerusubalu <nerusubalu@gmail.com>
2023-07-31 18:50:35 +0530

    Changes varible name

--- Introduction/uniqueness.py
+++ Introduction/uniqueness.py
@@ -5,24 +5,24 @@ Eg: [1,2,3,4,5f]
 
 
 #Running time of O(n^2)
-def unique(S):
-    n = len(S)
-    for i in range(n):
+def unique(seq):
+    n = len(seq)
+    for i in range(n): 
         for j in range(i+1, n):
-            if S[i]==S[j]:
+            if seq[i]==seq[j]:
                 return False
     return True
 
-#Running time of O(n)
-def unique_v2(S):
-    n = len(S)
-    S.sort()
+#Running time of O(nlogn)
+def unique_v2(seq):
+    n = len(seq)
+    seq.sort()
     for i in range(1,n):
-        if S[i]==S[i-1]:
+        if seq[i]==seq[i-1]:
             return False
     return True
 
 
-S = [1,2,34,5,6,7]
-print(unique(S))
-print(unique_v2(S))
+seq = [1,2,34,5,6,7]
+print(unique(seq))
+print(unique_v2(seq))
