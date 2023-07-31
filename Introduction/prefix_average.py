891d6f748c4da5dd1efcac17565975bb22b08ef2
nerusubalu <nerusubalu@gmail.com>
2023-07-31 18:26:43 +0530

    adds prefix average question

---
+++ Introduction/prefix_average.py
@@ -0,0 +1,32 @@
+'''
+Write a program to calculate the prefix sum
+Eg: 
+[1,2,3,4,5]
+[1, 3, 6, 10, 15]
+'''
+
+#O(n^2) Solution
+def prefix_average(arr):
+    n = len(arr)
+    sum_list = []
+    for i in range(n):
+        temp = 0
+        for j in range(i+1):
+            temp+=arr[j]
+        sum_list.append(temp/(i+1))
+    
+    return sum_list
+
+#O(n) Solution
+def prefix_average_v2(arr):
+    n = len(arr)
+    sum_list = []
+    sum = 0
+    for i in range(n):
+        sum+=arr[i]
+        sum_list.append(sum/(i+1))
+
+    return sum_list
+
+arr = [1,2,3,4,5]
+print(prefix_average_v2(arr))
